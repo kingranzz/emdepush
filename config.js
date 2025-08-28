0c254800c4cbd12255ff82ba7dd3c07782967fda
kingranzz <156513688+kingranzz@users.noreply.github.com>
2025-08-29 00:46:55 +0800

    Add files via upload

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 module.exports = {
-  BOT_TOKEN: "7549474841:AAG78UjaCRBwE63TNlNst_ZEc-hjJcwREjU", // TOKEN
-  ID_OWNER: "5965707646", // ID
+  BOT_TOKEN: "bot_token", // TOKEN
+  ID_OWNER: "id_lu", // ID
 };
